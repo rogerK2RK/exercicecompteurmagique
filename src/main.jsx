4745cc18e8f89924d66aaa87b628c1d96ff2a817
Roger Retita <roger.retita@gmail.com>
2024-11-19 12:20:42 +0100

    change position component dossier

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MagicCounter from './assets/component/MagicCounter'
-import Timer from './assets/component/Timer'
-import ResponsiveBackground from './assets/component/ResponsiveBackground'
+import MagicCounter from './component/MagicCounter'
+import Timer from './component/Timer'
+import ResponsiveBackground from './component/ResponsiveBackground'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
